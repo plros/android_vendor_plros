7be5030b829a022f7a63f1ffdd994a0258f3d2f5
Adithya R <gh0strider.2k18.reborn@gmail.com>
2023-06-03 14:20:21 +0530

    v_plros: Inherit AOSPA's face unlock implementation
    Change-Id: Ib1042d40b3d44fbde6eda8d6635fcb720e4b8d4b

--- plros.mk
+++ plros.mk
@@ -21,6 +21,20 @@ PRODUCT_PACKAGES += \
    Cromite \
    GameSpace
 
+# Face Unlock
+TARGET_FACE_UNLOCK_SUPPORTED ?= $(TARGET_SUPPORTS_64_BIT_APPS)
+
+ifeq ($(TARGET_FACE_UNLOCK_SUPPORTED),true)
+PRODUCT_PACKAGES += \
+    ParanoidSense
+
+PRODUCT_SYSTEM_EXT_PROPERTIES += \
+    ro.face.sense_service.enabled=true
+
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.biometrics.face.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.biometrics.face.xml
+endif
+
 # Fonts
 PRODUCT_PACKAGES += \
     FontCantarellOverlay \
