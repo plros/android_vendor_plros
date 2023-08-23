26fd04600019a717702ec17e4e1ee148d1148ff3
Whot1966 <Whot1966@users.noreply.github.com>
2023-08-23 13:38:44 +0000

    v_plros: Add remix display version * Use this in about phone instead off default LineageOS display version

--- build/core/main_version.mk
+++ build/core/main_version.mk
@@ -1,4 +1,5 @@
 # Remix properties
 ADDITIONAL_SYSTEM_PROPERTIES += \
     ro.remix.revision=$(REMIX_REVISION_TITLE) \
-    ro.remix.revision.url=$(REMIX_REVISION_SUMMARY)
+    ro.remix.revision.url=$(REMIX_REVISION_SUMMARY) \
+    ro.remix.display.version=$(REMIX_DISPLAY_VERSION)
